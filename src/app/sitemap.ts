178eff348e2971af88a284734e624f725f1b22c3
Tejas R <73103280+reddy123-1@users.noreply.github.com>
2026-06-16 15:07:56 +0530

    Update sitemap URL to new domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://tejasr.dev",
+      url: "https://tejasreddy.vercel.app",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
